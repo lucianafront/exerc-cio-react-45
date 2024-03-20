bd501aa5a05e53b216ed448466a4496c15c8826b
Luciana de Souza Felix <luciana.quimicatec@outlook.com>
2024-03-20 10:04:56 -0300

    exercicio46

--- src/components/Login/LoginControl.js
+++ src/components/Login/LoginControl.js
@@ -1,47 +1,58 @@
-import React from 'react'
-import UserGreeting from './index'
+import React from "react";
+import UserGreeting from "./index";
 
-export default class LoginControl extends React.Component{
+export default class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props)
+    this.handleLogin = this.handleLogin.bind(this);
 
-        this.handleLogin=this.handleLogin.bind(this)
+    this.handleLogout = this.handleLogout.bind(this);
 
-        this.handlogout=this.handLogout(this)
+    this.handleName = this.handleName.bind(this);
 
-        this.state={
-            isLoggedIn:false
-        }
+    this.state = {
+      isLoggedIn: false,
+      name: "",
+    };
+  }
 
+  handleLogin() {
+    this.setState({ isLoggedIn: true });
+  }
 
+  handleLogout() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  handleName(name) {
+    this.setState({ name: name });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    let button;
+
+    if (isLoggedIn) {
+      button = <button onClick={this.handleLogout}>Sair</button>;
+    } else {
+      button = (
+        <div>
+          <input
+            placeholder="digite seu nome"
+            onChange={(e) => this.handleName(e.target.value)}
+          />
+          <button onClick={this.handleLogin}>Acessar</button>
+        </div>
+      );
     }
-    handleLogin(){
-        this.setState({isLoggedIn:true})
 
-    }
-    handlogout(){
-        this.setState({isLoggedIn:false})
-
-    }
-
-    render(){
-        const isLoggedIn = this.state.isLoggedIn
-
-        let button
-
-        if (isLoggedIn) {
-            button= <button onClick={this.handLogout}>sair</button>    
-        }else{
-            button=<button onClick={this.handLogout}>Acessar</button>
-        }
-        return(
-            <div>
-                <UserGreeting isLoggedIn={isLoggedIn}/>
-                {button}
-
-            </div>
-        )
-        
-    }
+    return (
+      <div>
+        <UserGreeting isLoggedIn={isLoggedIn} name={this.state.name} />
+        {button}
+      </div>
+    );
+  }
 }
